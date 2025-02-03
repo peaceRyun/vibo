8e315e778866b4bbba5dde6a8c4ee2a0c9c0592f
jineeds <jineeds94@gmail.com>
2025-02-04 05:46:30 +0900

    [design] 멤버쉽 페이지 스타일 적용

--- src/pages/member/Membership.jsx
+++ src/pages/member/Membership.jsx
@@ -1,67 +1,84 @@
 // 멤버쉽(패스) 구매 페이지
 import { Container } from '../../common/style';
-import { MembershipBanner } from './style';
+import {
+  FeatureList,
+  MembershipBanner,
+  PaymentButton,
+  Plan,
+  PlanRow,
+  PlanWrapper,
+  SubscriptionContainer,
+} from './style';
 
 const Membership = () => {
   return (
     <>
       {/* 메인 배너 */}
-      <Container />
       <MembershipBanner>
-        <div className="banner">
+        <img src="/member/member_banner.PNG" alt="membershipbanner" />
+        <div className="banner-content">
           <h1>VIBO 여기서 지금 스트리밍중</h1>
           <p>다양한 콘텐츠를 합리적인 멤버십으로 즐겨보세요.</p>
-          <img src="/member/member_banner.PNG" alt="membershipbanner" />
         </div>
       </MembershipBanner>
       <section>
-        <div>
-          <h2>이용권 구독</h2>
-          <p>
-            전체 이용권 정보를 보시려면 <br /> 하단 [모든 이용권 보기] 버튼을 선택하세요.
-          </p>
-        </div>
-        <div>
-          <h2>베이직</h2>
-          <h2>스탠다드</h2>
-          <h2>프리미엄</h2>
-        </div>
-        <div>
-          <ul>
-            <li>월간요금</li>
-            <li>프로필 추가</li>
-            <li>풀영상시청</li>
-            <li>댓글 / 평점</li>
-            <li>VIBO PLUS 이용권</li>
-          </ul>
-        </div>
-        <div>
-          <ul>
-            <li>무료</li>
-            <li>1개</li>
-            <li>x</li>
-            <li>x</li>
-            <li>x</li>
-          </ul>
+        <SubscriptionContainer>
+          <div>
+            <h2>이용권 구독</h2>
+            <p>
+              전체 이용권 정보를 보시려면 <br /> 하단 [모든 이용권 보기] 버튼을 선택하세요.
+            </p>
 
-          <ul>
-            <li>5,500원</li>
-            <li>3개</li>
-            <li>v</li>
-            <li>v</li>
-            <li>x</li>
-          </ul>
+            <ul className="list">
+              <li>월간 요금</li>
+              <li>프로필 추가</li>
+              <li>풀영상시청</li>
+              <li>댓글 / 평점</li>
+              <li>VIBO PLUS 이용권</li>
+            </ul>
+          </div>
+          <PlanWrapper>
+            <Plan className="basic">
+              <h2>베이직</h2>
+            </Plan>
+            <FeatureList>
+              <li>무료</li>
+              <li>1개</li>
+              <li>x</li>
+              <li>x</li>
+              <li>x</li>
+            </FeatureList>
+          </PlanWrapper>
 
-          <ul>
-            <li>9,900원</li>
-            <li>3개</li>
-            <li>v</li>
-            <li>v</li>
-            <li>v</li>
-          </ul>
-        </div>
+          <PlanWrapper>
+            <Plan className="standard">
+              <h2>스탠다드</h2>
+            </Plan>
+            <FeatureList>
+              <li>5,500원</li>
+              <li>3개</li>
+              <li>✔</li>
+              <li>✔</li>
+              <li>x</li>
+            </FeatureList>
+          </PlanWrapper>
+
+          <PlanWrapper>
+            <Plan className="premium">
+              <h2>프리미엄</h2>
+            </Plan>
+            <FeatureList>
+              <li>9,900원</li>
+              <li>3개</li>
+              <li>✔</li>
+              <li>✔</li>
+              <li>✔</li>
+            </FeatureList>
+          </PlanWrapper>
+        </SubscriptionContainer>
+
+        <PaymentButton>결제하기</PaymentButton>
       </section>
-      <button>결제하기</button>
     </>
   );
 };
--- src/pages/member/style.js
+++ src/pages/member/style.js
@@ -1,13 +1,116 @@
 import styled from 'styled-components';
 
 export const MembershipBanner = styled.div`
-  .banner {
-    display: flex;
+  position: relative;
+  text-align: center;
+  color: white;
+  height: 300px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  overflow: hidden;
+
+  img {
     position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    z-index: -1;
+  }
+
+  .banner-content {
+    position: relative;
+    z-index: 1;
+
+    h1 {
+      font-size: 60px;
+      font-weight: bold;
+    }
+
+    p {
+      font-size: 40px;
+    }
   }
 `;
 
-// position: absolute;
-//   top: 50%;
-//   left: 50%;
-//   transform: translate(-50%, -50%);
+export const SubscriptionContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-top: 20px;
+  .list {
+    margin-top: 3.75rem;
+  }
+  li {
+    font-size: 20px;
+    padding: 10px 0;
+    /* border-top: 1px solid gray; */
+    text-align: left;
+  }
+`;
+
+export const PlanWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  padding: 0 10px;
+  position: relative;
+
+  &:not(:last-child)::after {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    width: 2px;
+    height: 100%;
+    background: gray;
+  }
+`;
+
+export const Plan = styled.div`
+  border: 1px solid cyan;
+  padding: 20px;
+  border-radius: 10px;
+  text-align: center;
+  width: 100%;
+  &:hover {
+    background: cyan;
+    color: black;
+  }
+`;
+
+export const FeatureList = styled.ul`
+  font-size: 20px;
+  margin-top: 3.75rem;
+  list-style: none;
+  padding: 0;
+  width: 100%;
+  li {
+    padding: 10px 0;
+    /* border-top: 1px solid gray; */
+    text-align: center;
+  }
+`;
+
+export const PaymentButton = styled.button`
+  display: block;
+  width: 200px;
+  margin: 20px auto;
+  padding: 10px;
+  background: cyan;
+  color: black;
+  font-size: 16px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+export const PlanRow = styled.div`
+  /* display: flex;
+  justify-content: space-between;
+  align-items: flex-start; */
+  /* width: 100%; */
+`;
