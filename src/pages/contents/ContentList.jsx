068b15a4fcb21bd6f1242af5cfee24d5f0cca471
peaceRyun <dbsrn1110@gmail.com>
2025-02-05 15:25:58 +0900

    [design] 더보기 페이지 title 사이즈 수정 / card의 불필요한 제목 제거

--- src/pages/contents/ContentList.jsx
+++ src/pages/contents/ContentList.jsx
@@ -1,6 +1,6 @@
 import { Container } from '../../components/contents/style';
 import Card from '../../ui/card/Card';
-import { GridContainer, Grid, H2 } from './style';
+import { GridContainer, Grid, Title } from './style';
 
 const ContentList = () => {
     const shows = Array(11).fill({
@@ -12,7 +12,10 @@ const ContentList = () => {
         <Container>
             <GridContainer>
                 <Grid>
-                    <H2>xx님이 좋아할만한 예능</H2>
+                    <Title>
+                        <div className='firstS'>xx님이</div>
+                        <div className='secondS'>좋아할만한 예능</div>
+                    </Title>
                     {shows.map((show, index) => (
                         <Card key={index} title={show.title} image={show.image} />
                     ))}
--- src/ui/card/Card.jsx
+++ src/ui/card/Card.jsx
@@ -1,12 +1,9 @@
-import { CardWrapper, CardImage, CardOverlay, CardTitle } from './style';
+import { CardWrapper, CardImage } from './style';
 
 const Card = ({ title, image }) => {
     return (
         <CardWrapper>
             <CardImage src={image} alt={title} />
-            <CardOverlay>
-                <CardTitle>{title}</CardTitle>
-            </CardOverlay>
         </CardWrapper>
     );
 };
