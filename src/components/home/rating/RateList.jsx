6e22418693bab262435b9d9146fa1f84872fa74f
jineeds <jineeds94@gmail.com>
2025-02-07 00:59:09 +0900

    [design] components-home-rating 아치형 스와이퍼 배경 수정/ 이미지 개별 적용 / 숫자 그라데이션 구현

--- src/components/home/rating/RateList.jsx
+++ src/components/home/rating/RateList.jsx
@@ -6,76 +6,88 @@ import { Pagination } from 'swiper/modules';
 import RateItem from './RateItem';
 
 const RateList = () => {
-    const [activeIndex, setActiveIndex] = useState(0); // 가장 왼쪽 슬라이드 인덱스 추적
+  const [activeIndex, setActiveIndex] = useState(0); // 가장 왼쪽 슬라이드 인덱스 추적
 
-    return (
-        <Section>
-            <RateInfo>
-                <RateTitle>
-                    20대 <br /> 인기 드라마
-                </RateTitle>
-            </RateInfo>
-            <RateSwiper
-                spaceBetween={83}
-                pagination={{ clickable: true }}
-                slidesPerView={4}
-                modules={[Pagination]}
-                onSlideChange={(swiper) => setActiveIndex(swiper.activeIndex)} // 슬라이드 변경 시 인덱스 업데이트
-            >
-                {[...Array(10)].map((_, index) => (
-                    <SwiperSlide key={index} className={index === activeIndex ? 'main-slide' : ''}>
-                        <RateItem />
-                    </SwiperSlide>
-                ))}
-            </RateSwiper>
-        </Section>
-    );
+  const images = [
+    '/mainhome/weeklyitem_01.webp',
+    '/mainhome/weeklyitem_02.webp',
+    '/mainhome/weeklyitem_03.webp',
+    '/mainhome/weeklyitem_04.webp',
+    '/mainhome/weeklyitem_05.webp',
+  ];
+
+  return (
+    <Section>
+      <RateInfo>
+        <RateTitle>
+          20대 <br /> 인기 드라마
+        </RateTitle>
+      </RateInfo>
+      <RateSwiper
+        spaceBetween={50}
+        pagination={{ clickable: true }}
+        slidesPerView={5}
+        modules={[Pagination]}
+        onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)} // 슬라이드 변경 시 인덱스 업데이트
+        // centeredSlides={true}
+        // loop={true}
+      >
+        {/* {[...Array(10)].map((_, index) => ( */}
+        {images.map((imgSrc, index) => (
+          <SwiperSlide key={index} className={index === activeIndex ? 'main-slide' : ''}>
+            <RateItem image={imgSrc} rank={index + 1} isActive={index === activeIndex} />
+          </SwiperSlide>
+        ))}
+      </RateSwiper>
+    </Section>
+  );
 };
 
 export default RateList;
 
 const Section = styled.section`
-    display: flex;
-    align-items: center;
-    position: relative;
-    gap: 5px;
+  display: flex;
+  align-items: center;
+  position: relative;
+  gap: 20px;
 `;
 
 const RateInfo = styled.div`
-    flex: 1;
-    display: flex;
-    flex-direction: column;
-    justify-content: space-between;
-    height: 443px;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 443px;
 `;
 
 const RateTitle = styled.h3`
-    color: white;
-    font-size: 40px;
-    font-weight: bold;
-    line-height: 1.5;
-    margin: 0;
+  color: white;
+  font-size: 40px;
+  font-weight: bold;
+  line-height: 1.5;
+  margin: 0;
+  text-align: left;
 `;
 
 const RateSwiper = styled(Swiper)`
-    flex: 3;
+  flex: 3;
+  height: 535.62px;
+
+  .swiper-slide {
+    width: 300px !important;
+    height: 443px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden;
+    border-radius: 8px;
+    transition: all 0.3s ease;
+  }
+
+  .main-slide {
+    width: 371.57px !important;
     height: 535.62px;
-
-    .swiper-slide {
-        width: 300px !important;
-        height: 443px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        overflow: hidden;
-        border-radius: 8px;
-        transition: all 0.3s ease;
-    }
-
-    .main-slide {
-        width: 371.57px !important;
-        height: 535.62px;
-        border-radius: 226.5px 226.5px 8px 8px;
-        background-color: #000;
-    }
+    border-radius: 226.5px 226.5px 8px 8px;
+    background-color: #000;
+  }
 `;
