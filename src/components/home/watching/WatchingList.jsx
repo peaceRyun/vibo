10a44fc40b2abf7095094a841877709483ebe192
jineeds <jineeds94@gmail.com>
2025-02-06 17:54:18 +0900

    [design] components-home-WatchingList 슬라이드 화면 넘침 오류 해결 / 컨테이너 max-width 1820px 설정으로 추가적용 / swiperslide 크기 조정

--- src/components/home/watching/WatchingList.jsx
+++ src/components/home/watching/WatchingList.jsx
@@ -3,26 +3,34 @@ import 'swiper/css';
 import styled from 'styled-components';
 import { Pagination } from 'swiper/modules';
 import WatchingItem from './WatchingItem';
+import { MainContainer } from '../../../pages/home/main/style';
 
 const WatchingList = () => {
   return (
-    <Section>
-      <LiveTitle>실시간 인기 LIVE</LiveTitle>
-      <LiveSwiper slidesPerView={3.8} spaceBetween={16} pagination={{ clickable: true }} modules={[Pagination]}>
-        {[...Array(5)].map((_, index) => (
-          <SwiperSlide style={{ overflow: 'visible' }} key={index}>
-            <WatchingItem />
-          </SwiperSlide>
-        ))}
-      </LiveSwiper>
-    </Section>
+    <MainContainer>
+      <LiveContainer>
+        <LiveTitle>실시간 인기 LIVE</LiveTitle>
+        <LiveSwiper slidesPerView={3.8} spaceBetween={16} pagination={{ clickable: true }} modules={[Pagination]}>
+          {[...Array(5)].map((_, index) => (
+            <SwiperSlide key={index}>
+              {/* style={{ overflow: 'visible' }} */}
+              <WatchingItem />
+            </SwiperSlide>
+          ))}
+        </LiveSwiper>
+      </LiveContainer>
+    </MainContainer>
   );
 };
 
 export default WatchingList;
 
 // Styled Components
-const Section = styled.section``;
+const LiveContainer = styled.section`
+  width: 100%;
+  max-width: 1820px;
+  margin: 0 auto;
+`;
 
 const LiveTitle = styled.h3`
   color: white;
@@ -32,8 +40,11 @@ const LiveTitle = styled.h3`
 `;
 
 const LiveSwiper = styled(Swiper)`
+  max-width: 100%;
   padding-bottom: 20px;
-  overflow: visible;
+  overflow: hidden;
+  /* padding-bottom: 20px;
+  overflow: visible; */
   .swiper-slide {
     border-radius: 8px;
     width: auto;
