4cbd864903378a2efed798c3c8e9ca6aaf81dec9
jineeds <jineeds94@gmail.com>
2025-02-07 14:42:40 +0900

    [design]시리즈, 무비 홈 순위 섹션 오류 발생 해결

--- src/components/home/popularlank/style.js
+++ src/components/home/popularlank/style.js
@@ -1,9 +1,11 @@
 import styled from 'styled-components';
 
 export const ListWrapper = styled.div`
+  display: flex;
   flex: 3;
   height: 535.62px;
   /* overflow: visible !important; */
+
   .swiper-slide {
     width: 300px !important;
     height: 443px;
@@ -20,9 +22,30 @@ export const PopularSection = styled.div`
   display: flex;
   align-items: center;
   position: relative;
+  max-width: 1820px;
+  padding: 0 20px;
   gap: 20px;
   margin-top: 100px;
 `;
+
+export const PopularTitle = styled.h2`
+  color: white;
+  width: 23rem;
+  font-size: 40px;
+  font-weight: bold;
+  line-height: 1.5;
+  margin: 0;
+  text-align: left;
+`;
+
+export const PopularInfo = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 443px;
+`;
+
 // ---------------------------------아이템 스타일링
 export const ItemWrapper = styled.div`
   position: relative;
@@ -34,8 +57,8 @@ export const RankingNumber = styled.span`
   position: absolute;
   bottom: -30px;
   left: 10px;
-  font-size: 64px;
-  font-weight: bold;
+  font-size: 180px;
+  font-weight: 800;
   color: rgba(255, 255, 255, 0.3);
   z-index: 1;
 `;
