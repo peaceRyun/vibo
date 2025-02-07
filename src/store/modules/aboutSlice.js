001fc556a142c1f26d68a0ed3a953833d603930b
peaceRyun <dbsrn1110@gmail.com>
2025-02-07 15:20:25 +0900

    [feat] redux 폴더 생성1

---
+++ src/store/modules/aboutSlice.js
@@ -0,0 +1,13 @@
+// import { createSlice } from '@reduxjs/toolkit';
+// import 참조변수 from '../../assets/api/데이터';
+
+// const initialState = {};
+
+// export const xxSlice = createSlice({
+//     name: 'xx',
+//     initialState,
+//     reducers: {},
+// });
+
+// export const {} = xxSlice.actions;
+// export default xxSlice.reducer;
---
+++ src/store/modules/tmSlice.js
@@ -0,0 +1,28 @@
+// 비동기 받는 slice
+
+// import { createSlice } from '@reduxjs/toolkit';
+// import { getxxx } from './getThunk'
+
+// const initialState = {
+// };
+
+// export const 이름Slice = createSlice({
+//     name: '이름',
+//     initialState,
+//     reducers: {},
+//     extraReducers: (builder) => {
+//         builder
+//             .addCase(getxx.pending, (state, action) => {
+
+//             })
+//             .addCase(getxx.fulfilled, (state, action) => {
+
+//             })
+//             .addCase(getxx.rejected, (state, action) => {
+
+//             });
+//     },
+// });
+
+// export const 이름Actions = 이름Slice.actions;
+// export default 이름Slice.reducer;
