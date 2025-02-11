c9cd884eedb0dd02dcdc5616a71815e3f1fdb0fc
2mighytMt <kangsan5262@gmail.com>
2025-02-11 17:32:25 +0900

    [fix]스와이퍼 밀림 해결

--- src/components/home/rating/RateList.jsx
+++ src/components/home/rating/RateList.jsx
@@ -6,109 +6,107 @@ import styled from 'styled-components';
 import RateItem from './RateItem';
 
 const RateList = () => {
-  const [activeIndex, setActiveIndex] = useState(0); // 가장 왼쪽 슬라이드 인덱스 추적
+    const [activeIndex, setActiveIndex] = useState(0); // 가장 왼쪽 슬라이드 인덱스 추적
 
-  const images = [
-    '/mainhome/weeklyitem_01.webp',
-    '/mainhome/weeklyitem_02.webp',
-    '/mainhome/weeklyitem_03.webp',
-    '/mainhome/weeklyitem_04.webp',
-    '/mainhome/weeklyitem_05.webp',
-    '/mainhome/weeklyitem_01.webp',
-    '/mainhome/weeklyitem_02.webp',
-    '/mainhome/weeklyitem_03.webp',
-    '/mainhome/weeklyitem_04.webp',
-    '/mainhome/weeklyitem_05.webp',
-    '/mainhome/weeklyitem_01.webp',
-    '/mainhome/weeklyitem_02.webp',
-    '/mainhome/weeklyitem_03.webp',
-    '/mainhome/weeklyitem_04.webp',
-    '/mainhome/weeklyitem_05.webp',
-  ];
+    const images = [
+        '/mainhome/weeklyitem_01.webp',
+        '/mainhome/weeklyitem_02.webp',
+        '/mainhome/weeklyitem_03.webp',
+        '/mainhome/weeklyitem_04.webp',
+        '/mainhome/weeklyitem_05.webp',
+        '/mainhome/weeklyitem_01.webp',
+        '/mainhome/weeklyitem_02.webp',
+        '/mainhome/weeklyitem_03.webp',
+        '/mainhome/weeklyitem_04.webp',
+        '/mainhome/weeklyitem_05.webp',
+        '/mainhome/weeklyitem_01.webp',
+        '/mainhome/weeklyitem_02.webp',
+        '/mainhome/weeklyitem_03.webp',
+        '/mainhome/weeklyitem_04.webp',
+        '/mainhome/weeklyitem_05.webp',
+    ];
 
-  return (
-    <Section>
-      <RateInfo>
-        <RateTitle>
-          20대 <br /> 인기 드라마
-        </RateTitle>
-      </RateInfo>
+    return (
+        <Section>
+            <RateInfo>
+                <RateTitle>
+                    20대 <br /> 인기 드라마
+                </RateTitle>
+            </RateInfo>
 
-      <RateSwiper
-        spaceBetween={50}
-        pagination={{ clickable: true }}
-        slidesPerView={4}
-        // modules={[Pagination]}
-        onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)} // 슬라이드 변경 시 인덱스 업데이트
-        // centeredSlides={true}
-        // loop={true}
-      >
-        {/* {[...Array(10)].map((_, index) => ( */}
-        {images.map((imgSrc, index) => (
-          <SwiperSlide key={index} className={index === activeIndex ? 'main-slide' : ''}>
-            <RateItem image={imgSrc} rank={index + 1} isActive={index === activeIndex} />
-          </SwiperSlide>
-          // <SwiperSlide key={index} className={index === activeIndex ? 'main-slide' : ''}>
-          //   <RateItem image={imgSrc} rank={index + 1} isActive={index === activeIndex} />
-          // </SwiperSlide>
-        ))}
-      </RateSwiper>
-    </Section>
-  );
+            <RateSwiper
+                spaceBetween={50}
+                pagination={{ clickable: true }}
+                slidesPerView={4}
+                // modules={[Pagination]}
+                onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)} // 슬라이드 변경 시 인덱스 업데이트
+                // centeredSlides={true}
+                // loop={true}
+            >
+                {/* {[...Array(10)].map((_, index) => ( */}
+                {images.map((imgSrc, index) => (
+                    <SwiperSlide key={index} className={index === activeIndex ? 'main-slide' : ''}>
+                        <RateItem image={imgSrc} rank={index + 1} isActive={index === activeIndex} />
+                    </SwiperSlide>
+                    // <SwiperSlide key={index} className={index === activeIndex ? 'main-slide' : ''}>
+                    //   <RateItem image={imgSrc} rank={index + 1} isActive={index === activeIndex} />
+                    // </SwiperSlide>
+                ))}
+            </RateSwiper>
+        </Section>
+    );
 };
 
 export default RateList;
 
 const Section = styled.section`
-  display: flex;
-  align-items: flex-start;
-  position: relative;
-  /* gap: 20px; */
-  /* margin-top: 100px; */
+    display: flex;
+    align-items: flex-start;
+    position: relative;
+    /* gap: 20px; */
+    /* margin-top: 100px; */
 `;
 
 const RateInfo = styled.div`
-  flex: 1;
-  display: flex;
+    flex: 1;
+    display: flex;
 
-  flex-direction: column;
-  justify-content: space-between;
-  height: 443px;
-  width: 443px;
+    flex-direction: column;
+    justify-content: space-between;
+    height: 443px;
+    width: 443px;
 `;
 
 const RateTitle = styled.h3`
-  color: white;
-  font-size: 40px;
-  font-weight: bold;
-  line-height: 1.5;
-  margin: 0;
-  text-align: left;
+    color: white;
+    font-size: 40px;
+    font-weight: bold;
+    line-height: 1.5;
+    margin: 0;
+    text-align: left;
 `;
 
 const RateSwiper = styled(Swiper)`
-  flex: 3;
-  height: 535.62px;
-  /* margin-bottom: 40px; */
-  display: flex;
-
-  .swiper-slide {
-    width: 300px !important;
-    height: 443px;
-    display: flex;
-    justify-content: center; /* 내부 이미지 중앙 정렬 */
-    overflow: hidden;
-    border-radius: 8px;
-    transition: all 0.3s ease;
-    position: relative;
-  }
-
-  .main-slide {
-    width: 371.57px !important;
+    flex: 3;
     height: 535.62px;
-    border-radius: 226.5px 226.5px 8px 8px;
-    background-color: #000;
-    /* position: relative; */
-    /* overflow: visible; */
-  }
+    display: flex;
+    width: 100%;
+
+    .swiper-slide {
+        width: 300px;
+        height: 443px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        overflow: hidden;
+        border-radius: 8px;
+        transition: all 0.3s ease;
+    }
+
+    .main-slide {
+        width: 370px !important; /* 강제로 370px로 설정 */
+        height: 535.62px;
+        border-radius: 226.5px 226.5px 8px 8px;
+        background-color: #000;
+    }
 `;
--- src/components/home/rating/style.js
+++ src/components/home/rating/style.js
@@ -1,35 +1,34 @@
 import styled from 'styled-components';
 
 export const RateItemContainer = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
+    position: relative;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    overflow: hidden; /* 이미지가 튀어나오지 않도록 설정 */
 `;
 
 export const RateItemImg = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  position: absolute;
-  border-radius: ${({ isFirst }) => (isFirst ? '200px 200px 8px 8px' : '8px')};
-  transition: border-radius 0.5s ease-in-out;
-  align-items: center;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+    border-radius: ${({ isFirst }) => (isFirst ? '200px 200px 8px 8px' : '8px')};
+    transition: all 0.5s ease-in-out; /* 모든 스타일의 변화에 애니메이션 적용 */
 `;
 
+/* 숫자 위치를 유동적으로 조정 */
 export const RankNumber = styled.div`
-  position: relative;
-  bottom: -50px;
-  left: -30px;
-  z-index: 10;
-  font-size: ${({ isFirst }) => (isFirst ? '300px' : '200px')};
-  font-weight: 900;
-  color: white;
-  /* opacity: ${({ isFirst }) => (isFirst ? '1' : '0.5')}; */
-  background: linear-gradient(to bottom, rgba(255, 255, 255, 0.9), rgba(0, 0, 0, 0.1));
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  transition: all 0.5s ease-in-out;
+    position: absolute;
+    bottom: 10px;
+    left: 20px;
+    z-index: 10;
+    font-size: ${({ isFirst }) => (isFirst ? '300px' : '200px')};
+    font-weight: 900;
+    color: white;
+    background: linear-gradient(to left, #fff, rgba(0, 0, 0, 0.1));
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    transition: all 0.5s ease-in-out;
 `;
