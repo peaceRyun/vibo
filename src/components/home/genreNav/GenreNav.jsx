c9440a2261d213ab10fe42842a169d954c0be13a
jineeds <jineeds94@gmail.com>
2025-02-13 13:12:25 +0900

    [design] components -home - viboOnly/ genreNav / adBanner  메인 레이아웃 조정 // 영화, 시리즈 홈 - MoreButton 더보기 레이아웃 조정

--- src/components/home/genreNav/GenreNav.jsx
+++ src/components/home/genreNav/GenreNav.jsx
@@ -2,44 +2,45 @@ import { useState } from 'react';
 import { GenreContainer, GenreItem } from './style';
 
 const genres = [
-    { en: 'ACTION', ko: '액션' },
-    { en: 'COMEDY', ko: '코미디' },
-    { en: 'FANTASY', ko: '판타지' },
-    { en: 'ROMANCE', ko: '로맨스' },
-    { en: 'THRILLER', ko: '스릴러' },
-    { en: 'MUSICAL', ko: '뮤지컬' },
-    { en: 'ANIMATION', ko: '애니메이션' },
-    { en: 'NOIR', ko: '느와르' },
+  { en: 'ACTION', ko: '액션' },
+  { en: 'COMEDY', ko: '코미디' },
+  { en: 'FANTASY', ko: '판타지' },
+  { en: 'ROMANCE', ko: '로맨스' },
+  { en: 'THRILLER', ko: '스릴러' },
+  { en: 'MUSICAL', ko: '뮤지컬' },
+  { en: 'ANIMATION', ko: '애니메이션' },
+  { en: 'NOIR', ko: '느와르' },
 ];
 
 const defaultSelectedGenres = ['ACTION', 'MUSICAL', 'FANTASY', 'NOIR'];
 const defaultBorderGenres = ['THRILLER', 'COMEDY', 'ANIMATION', 'ROMANCE'];
 
 const GenreNav = () => {
-    const [selectedGenres, setSelectedGenres] = useState(defaultSelectedGenres);
-    const [borderGenres] = useState(defaultBorderGenres);
+  const [selectedGenres, setSelectedGenres] = useState(defaultSelectedGenres);
+  const [borderGenres] = useState(defaultBorderGenres);
 
-    const toggleGenre = (genre) => {
-        setSelectedGenres((prev) => (prev.includes(genre) ? prev.filter((g) => g !== genre) : [...prev, genre]));
-    };
+  const toggleGenre = (genre) => {
+    setSelectedGenres((prev) => (prev.includes(genre) ? prev.filter((g) => g !== genre) : [...prev, genre]));
+  };
 
-    return (
-        <GenreContainer>
-            {genres.map((genre) => (
-                <GenreItem
-                    key={genre.en}
-                    className={`
+  return (
+    <GenreContainer>
+      {genres.map((genre) => (
+        <GenreItem
+          key={genre.en}
+          className={`
                         ${selectedGenres.includes(genre.en) ? 'active' : ''}
                         ${borderGenres.includes(genre.en) ? 'border' : ''}
                     `}
-                    onClick={() => toggleGenre(genre.en)}
-                >
-                    <span className="en">{genre.en}</span>
-                    <span className="ko">{genre.ko}</span>
-                </GenreItem>
-            ))}
-        </GenreContainer>
-    );
+          onClick={() => toggleGenre(genre.en)}
+        >
+          <span className="en">{genre.en}</span>
+          <span className="ko">{genre.ko}</span>
+        </GenreItem>
+      ))}
+      {/* </div> */}
+    </GenreContainer>
+  );
 };
 
 export default GenreNav;
