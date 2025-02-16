680670034770352e16819a88af60adbd6a2d9f37
jineeds <jineeds94@gmail.com>
2025-02-17 00:00:03 +0900

    [design] home-live-live/watching 유튜브 더미자료로 동영상 재생 구현

--- src/components/home/live/LiveItem.jsx
+++ src/components/home/live/LiveItem.jsx
@@ -2,12 +2,25 @@
 // 메인홈, 라이브홈에만 있는 스와이퍼
 // 비슷한 콘텐츠
 // 영화, 시리즈 콘텐츠 공통
-import { CardContainer, CardImage, CardInfo, CardTitle, CardBcName } from './style';
+import { CardContainer, CardInfo, CardTitle, CardBcName, IframeWrapper, StyledIframe } from './style';
 
-const LiveItem = () => {
+const LiveItem = ({ videoUrl }) => {
+  // 테스트
+  // const embedUrl = videoUrl.includes('embed')
+  //   ? `${videoUrl}?autoplay=1&mute=1`
+  //   : videoUrl.replace('youtu.be/', 'www.youtube.com/embed/').split('?')[0] + '?autoplay=1&mute=1';
   return (
     <CardContainer>
-      <CardImage src="/mainhome/sample/LiveSample.png" alt="KCC 프로농구 tvN" />
+      <IframeWrapper>
+        <StyledIframe
+          src={videoUrl}
+          title="YouTube video player"
+          // frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        />
+      </IframeWrapper>
       <CardInfo>
         <CardTitle>KCC 프로농구</CardTitle>
         <CardBcName>tvN</CardBcName>
--- src/components/home/live/LiveList.jsx
+++ src/components/home/live/LiveList.jsx
@@ -4,13 +4,26 @@ import styled from 'styled-components';
 import LiveItem from './LiveItem';
 
 const LiveList = ({ title }) => {
+  const videoUrls = [
+    'https://www.youtube.com/embed/9z3HYlFcc6Q?autoplay=1&mute=1',
+    'https://www.youtube.com/embed/cw83AVjugmg?autoplay=1&mute=1',
+    'https://www.youtube.com/embed/MKX5W7mGUzo?autoplay=1&mute=1',
+    'https://www.youtube.com/embed/clqcIUYkbGQ?autoplay=1&mute=1',
+    'https://www.youtube.com/embed/v4iz9Mh8R-4?autoplay=1&mute=1',
+    'https://www.youtube.com/embed/9z3HYlFcc6Q?autoplay=1&mute=1',
+    'https://www.youtube.com/embed/cw83AVjugmg?autoplay=1&mute=1',
+    'https://www.youtube.com/embed/MKX5W7mGUzo?autoplay=1&mute=1',
+    'https://www.youtube.com/embed/clqcIUYkbGQ?autoplay=1&mute=1',
+    'https://www.youtube.com/embed/v4iz9Mh8R-4?autoplay=1&mute=1',
+  ];
+
   return (
     <LiveContainer>
       <StyledTitle>{title}</StyledTitle>
-      <StyledSwiper slidesPerView={4.2} spaceBetween={16} pagination={{ clickable: true }}>
-        {[...Array(5)].map((_, index) => (
+      <StyledSwiper slidesPerView={4.2} spaceBetween={16}>
+        {videoUrls.map((url, index) => (
           <SwiperSlide key={index}>
-            <LiveItem />
+            <LiveItem videoUrl={url} />
           </SwiperSlide>
         ))}
       </StyledSwiper>
