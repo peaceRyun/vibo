fbf222d23251c0f5948d66c66348105edf50c03d
peaceRyun <dbsrn1110@gmail.com>
2025-02-20 11:32:38 +0900

    [design(fix)] 기존 styled component위치 style.js로 옮김

--- src/components/home/rating/RateList.jsx
+++ src/components/home/rating/RateList.jsx
@@ -1,120 +1,73 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import styled from 'styled-components';
 import RateItem from './RateItem';
+import { LeftArea, RateTitle, RightArea, Section } from './style';
 
 const RateList = () => {
-  const [activeIndex, setActiveIndex] = useState(0);
+    const [activeIndex, setActiveIndex] = useState(0);
 
-  const images = [
-    '/mainhome/weeklyitem_01.webp',
-    '/mainhome/weeklyitem_02.webp',
-    '/mainhome/weeklyitem_03.webp',
-    '/mainhome/weeklyitem_04.webp',
-    '/mainhome/weeklyitem_05.webp',
-    '/mainhome/weeklyitem_01.webp',
-    '/mainhome/weeklyitem_02.webp',
-    '/mainhome/weeklyitem_03.webp',
-    '/mainhome/weeklyitem_04.webp',
-    '/mainhome/weeklyitem_05.webp',
-    '/mainhome/weeklyitem_01.webp',
-    '/mainhome/weeklyitem_02.webp',
-    '/mainhome/weeklyitem_03.webp',
-    '/mainhome/weeklyitem_04.webp',
-    '/mainhome/weeklyitem_05.webp',
-    '/mainhome/weeklyitem_01.webp',
-    '/mainhome/weeklyitem_02.webp',
-    '/mainhome/weeklyitem_03.webp',
-    '/mainhome/weeklyitem_04.webp',
-    '/mainhome/weeklyitem_05.webp',
-  ];
+    const images = [
+        '/mainhome/weeklyitem_01.webp',
+        '/mainhome/weeklyitem_02.webp',
+        '/mainhome/weeklyitem_03.webp',
+        '/mainhome/weeklyitem_04.webp',
+        '/mainhome/weeklyitem_05.webp',
+        '/mainhome/weeklyitem_01.webp',
+        '/mainhome/weeklyitem_02.webp',
+        '/mainhome/weeklyitem_03.webp',
+        '/mainhome/weeklyitem_04.webp',
+        '/mainhome/weeklyitem_05.webp',
+        '/mainhome/weeklyitem_01.webp',
+        '/mainhome/weeklyitem_02.webp',
+        '/mainhome/weeklyitem_03.webp',
+        '/mainhome/weeklyitem_04.webp',
+        '/mainhome/weeklyitem_05.webp',
+        '/mainhome/weeklyitem_01.webp',
+        '/mainhome/weeklyitem_02.webp',
+        '/mainhome/weeklyitem_03.webp',
+        '/mainhome/weeklyitem_04.webp',
+        '/mainhome/weeklyitem_05.webp',
+    ];
 
-  return (
-    <Section>
-      <LeftArea>
-        <RateTitle>인기 드라마</RateTitle>
-      </LeftArea>
+    return (
+        <Section>
+            <LeftArea>
+                <RateTitle>인기 드라마</RateTitle>
+            </LeftArea>
 
-      <RightArea>
-        <RateSwiper
-          spaceBetween={60} /* 슬라이드 간의 간격을 넉넉하게 설정 */
-          slidesPerView={3.5}
-          centeredSlides={false} /* 메인 슬라이드가 강조되도록 설정 */
-          onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
-          breakpoints={{
-            390: { slidesPerView: 1, spaceBetween: 50 },
-            600: { slidesPerView: 1.3, spaceBetween: 15 },
-            768: { slidesPerView: 2.3, spaceBetween: 15 },
-            1024: { slidesPerView: 3.3, spaceBetween: 15 },
-          }}
-        >
-          {images.map((imgSrc, index) => (
-            <SwiperSlide key={index}>
-              <RateItem image={imgSrc} rank={index + 1} isMainSlide={index === activeIndex} />
-            </SwiperSlide>
-          ))}
-        </RateSwiper>
-      </RightArea>
-    </Section>
-  );
+            <RightArea>
+                <RateSwiper
+                    spaceBetween={60} /* 슬라이드 간의 간격을 넉넉하게 설정 */
+                    slidesPerView={3.5}
+                    centeredSlides={false} /* 메인 슬라이드가 강조되도록 설정 */
+                    onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+                    breakpoints={{
+                        390: { slidesPerView: 1, spaceBetween: 50 },
+                        600: { slidesPerView: 1.3, spaceBetween: 15 },
+                        768: { slidesPerView: 2.3, spaceBetween: 15 },
+                        1024: { slidesPerView: 3.3, spaceBetween: 15 },
+                    }}
+                >
+                    {images.map((imgSrc, index) => (
+                        <SwiperSlide key={index}>
+                            <RateItem image={imgSrc} rank={index + 1} isMainSlide={index === activeIndex} />
+                        </SwiperSlide>
+                    ))}
+                </RateSwiper>
+            </RightArea>
+        </Section>
+    );
 };
 
 export default RateList;
 
-const Section = styled.section`
-  display: flex;
-  flex-direction: row;
-  gap: 40px;
-  height: 75vh;
-  /* padding: 60px 0 60px 0; */
-  @media (max-width: 1024px) {
-    flex-direction: column;
-    /* gap: 40px; */
-    height: 95vh;
-    padding: 40px 0 40px 0;
-  }
-  @media (max-width: 600px) {
-    /* padding:0px 0 0px 0; */
-    height: 100vh;
-  }
-`;
-
-const LeftArea = styled.div`
-  flex: 1;
-  /* padding-left: 60px;  */
-  display: flex;
-  align-items: center;
-`;
-
-const RightArea = styled.div`
-  padding-left: 85px;
-  flex: 3;
-  overflow: hidden; /* 왼쪽으로 넘치는 슬라이드를 숨김 */
-`;
-
-const RateTitle = styled.h3`
-  color: white;
-  line-height: 1.5;
-  margin: 0;
-  font-size: var(--title-xlarge-size); //32
-  font-weight: var(--title-xlarge-weight); //800
-  @media (max-width: 1024px) {
-    padding: 20px;
-    font-size: var(--title-large-mobile); //24
-    font-weight: var(--title-large-weigh); //700
-  }
-  @media (max-width: 600px) {
-    font-size: var(--title-small-mobile); //17
-    font-weight: var(--title-xsmall-weight); //700
-  }
-`;
-
 const RateSwiper = styled(Swiper)`
-  width: 100%;
-  overflow: visible;
-
-  .swiper-slide {
+    width: 100%;
     overflow: visible;
-  }
+
+    .swiper-slide {
+        overflow: visible;
+    }
 `;
--- src/components/home/rating/style.js
+++ src/components/home/rating/style.js
@@ -1,33 +1,81 @@
 import styled from 'styled-components';
 
+export const Section = styled.section`
+    display: flex;
+    flex-direction: row;
+    gap: 40px;
+    height: 75vh;
+    /* padding: 60px 0 60px 0; */
+    @media (max-width: 1024px) {
+        flex-direction: column;
+        /* gap: 40px; */
+        height: 95vh;
+        padding: 40px 0 40px 0;
+    }
+    @media (max-width: 600px) {
+        /* padding:0px 0 0px 0; */
+        height: 100vh;
+    }
+`;
+
+export const LeftArea = styled.div`
+    flex: 1;
+    /* padding-left: 60px;  */
+    display: flex;
+    align-items: center;
+`;
+
+export const RightArea = styled.div`
+    padding-left: 85px;
+    flex: 3;
+    overflow: hidden; /* 왼쪽으로 넘치는 슬라이드를 숨김 */
+`;
+
+export const RateTitle = styled.h3`
+    color: white;
+    line-height: 1.5;
+    margin: 0;
+    font-size: var(--title-xlarge-size); //32
+    font-weight: var(--title-xlarge-weight); //800
+    @media (max-width: 1024px) {
+        padding: 20px;
+        font-size: var(--title-large-mobile); //24
+        font-weight: var(--title-large-weigh); //700
+    }
+    @media (max-width: 600px) {
+        font-size: var(--title-small-mobile); //17
+        font-weight: var(--title-xsmall-weight); //700
+    }
+`;
+
 export const OuterContainer = styled.div`
-  position: relative;
-  width: ${({ $isMainSlide }) => ($isMainSlide ? '371.57px' : '255.88px')};
-  height: ${({ $isMainSlide }) => ($isMainSlide ? '535.62px' : '369.11px')};
-  overflow: visible;
+    position: relative;
+    width: ${({ $isMainSlide }) => ($isMainSlide ? '371.57px' : '255.88px')};
+    height: ${({ $isMainSlide }) => ($isMainSlide ? '535.62px' : '369.11px')};
+    overflow: visible;
 `;
 
 export const SlideImageContainer = styled.div`
-  width: 100%;
-  height: 100%;
-  overflow: hidden;
-  border-radius: ${({ $isMainSlide }) => ($isMainSlide ? '200px 200px 8px 8px' : '8px')};
-  position: relative;
-  transition: border-radius 1s ease;
-  @media (max-width: 1024px) {
-    width: 85%;
-    height: 85%;
-  }
-  @media (max-width: 600px) {
-    width: 65%;
-    height: 65%;
-  }
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+    border-radius: ${({ $isMainSlide }) => ($isMainSlide ? '200px 200px 8px 8px' : '8px')};
+    position: relative;
+    transition: border-radius 1s ease;
+    @media (max-width: 1024px) {
+        width: 85%;
+        height: 85%;
+    }
+    @media (max-width: 600px) {
+        width: 65%;
+        height: 65%;
+    }
 `;
 
 export const SlideImage = styled.img`
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 `;
 
 // export const RankNumber = styled.div`
@@ -47,50 +95,50 @@ export const SlideImage = styled.img`
 
 ////숫자 변경디자인 적용
 export const RankNumber = styled.div`
-  position: absolute;
-  width: 152.32px;
-  height: 333.92px;
+    position: absolute;
+    width: 152.32px;
+    height: 333.92px;
 
-  /* top: 50%;
+    /* top: 50%;
   left: 10px; */
-  top: 79%;
-  left: -72px;
-  transform: translateY(-50%) rotate(-0.12deg);
+    top: 79%;
+    left: -72px;
+    transform: translateY(-50%) rotate(-0.12deg);
 
-  font-family: 'NanumSquare Neo', sans-serif;
-  font-style: normal;
-  font-weight: 900;
-  font-size: 130px;
-  line-height: 500px;
+    font-family: 'NanumSquare Neo', sans-serif;
+    font-style: normal;
+    font-weight: 900;
+    font-size: 130px;
+    line-height: 500px;
 
-  background: linear-gradient(92.58deg, rgba(0, 0, 0, 0.5) 40.27%, rgba(255, 255, 255, 0.5) 89.41%);
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  background-clip: text;
-  text-fill-color: transparent;
+    background: linear-gradient(92.58deg, rgba(0, 0, 0, 0.5) 40.27%, rgba(255, 255, 255, 0.5) 89.41%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    background-clip: text;
+    text-fill-color: transparent;
 
-  -webkit-text-stroke: 5px white;
-  z-index: 10;
+    -webkit-text-stroke: 5px white;
+    z-index: 10;
 
-  ${({ $isMainSlide }) =>
-    $isMainSlide &&
-    `
+    ${({ $isMainSlide }) =>
+        $isMainSlide &&
+        `
         font-size: 200px;
         line-height: 400px;
         -webkit-text-stroke: 5px white; /* 🎯 메인 숫자는 더 굵은 테두리 적용 */
     `}
-  @media (max-width: 1024px) {
-    width: 90%;
-    top: 65%;
-    left: -150px;
-  }
+    @media (max-width: 1024px) {
+        width: 90%;
+        top: 65%;
+        left: -150px;
+    }
 
-  @media (max-width: 600px) {
-    font-size: 120px;
-    width: 60%;
-    top: 50%;
-    left: -100px;
-  }
+    @media (max-width: 600px) {
+        font-size: 120px;
+        width: 60%;
+        top: 50%;
+        left: -100px;
+    }
 `;
 `;
 
