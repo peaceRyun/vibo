6f8bb2f9d08e966c048ea19be884bc3653060452
peaceRyun <dbsrn1110@gmail.com>
2025-02-20 21:05:44 +0900

    [design(feat)] 마이페이지 시청내역 GSAP 적용.

---
+++ src/components/member/watch/SampleWatchingContent.jsx
@@ -0,0 +1,30 @@
+// 사용자 시청내역
+import { ContentSection, MovieCard, RecommendedMovies } from './samplestyle';
+
+const SampleWatchingContent = () => {
+    const movies = [
+        { src: '/member/mypage_01.webp' },
+        { src: '/member/mypage_02.webp' },
+        { src: '/member/mypage_03.webp' },
+        { src: '/member/mypage_04.webp' },
+    ];
+
+    return (
+        <div>
+            <ContentSection>
+                <h3>시청하신 콘텐츠가 없습니다.</h3>
+                <p>다양한 콘텐츠를 시청해보세요.</p>
+                <p>고객님의 취향에 맞추어 추천을 해드릴까요?</p>
+
+                {/* 추천 영화 카드 */}
+                <RecommendedMovies>
+                    {movies.map((movie, index) => (
+                        <MovieCard key={index} style={{ backgroundImage: `url(${movie.src})` }} />
+                    ))}
+                </RecommendedMovies>
+            </ContentSection>
+        </div>
+    );
+};
+
+export default SampleWatchingContent;
