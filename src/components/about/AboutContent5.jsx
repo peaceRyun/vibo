24f146ed5aad2cf7e4e9096adf7e70d9cea3dc7e
jineeds <jineeds94@gmail.com>
2025-02-23 22:07:56 +0900

    [design] about page 버튼, 컬러 통일화, FAQ dropdown 디자인 변경

--- src/components/about/AboutContent5.jsx
+++ src/components/about/AboutContent5.jsx
@@ -2,25 +2,25 @@ import { useNavigate } from 'react-router';
 import { LastSectionWrapper } from './style';
 
 const AboutContent5 = () => {
-    const navigate = useNavigate();
-    const onGoLogin = () => {
-        navigate('/login');
-    };
-    return (
-        <>
-            <LastSectionWrapper>
-                <img
-                    src='https://raw.githubusercontent.com/peaceRyun/vibostatic/refs/heads/main/public/mockup/about/logo 1.png'
-                    alt='VIBO 로그인'
-                    className='logo'
-                />
-                <h2>지금 함께 해 보세요.</h2>
-                <button className='login-btn' onClick={onGoLogin}>
-                    로그인
-                </button>
-            </LastSectionWrapper>
-        </>
-    );
+  const navigate = useNavigate();
+  const onGoLogin = () => {
+    navigate('/login');
+  };
+  return (
+    <>
+      <LastSectionWrapper>
+        <img
+          src="https://raw.githubusercontent.com/peaceRyun/vibostatic/refs/heads/main/public/mockup/about/logo 1.png"
+          alt="VIBO 로그인"
+          className="logo"
+        />
+        <h2>지금 함께 해 보세요.</h2>
+        <button className="login-btn" onClick={onGoLogin}>
+          로그인
+        </button>
+      </LastSectionWrapper>
+    </>
+  );
 };
 
 export default AboutContent5;
