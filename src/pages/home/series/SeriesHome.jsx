95202f8a8551c15297841c4be96fa26caadd037b
peaceRyun <dbsrn1110@gmail.com>
2025-02-24 21:25:14 +0900

    [feat] 시리즈홈 더보기 버튼 기능 구현. / 헤더 사용자 변경 구현.

--- src/pages/home/series/SeriesHome.jsx
+++ src/pages/home/series/SeriesHome.jsx
@@ -1,5 +1,4 @@
-// 시리즈 홈
-import React from 'react';
+import React, { useState } from 'react';
 import MainBanner from '../../../components/home/mainBanner/MainBanner';
 import { MainContainer, MainContent } from '../main/style';
 import CommonList from '../../../components/home/common/CommonList';
@@ -10,25 +9,36 @@ import PopularList from '../../../components/home/popularlank/PopularList';
 import RateList from '../../../components/home/rating/RateList';
 
 const SeriesHome = () => {
-  const seriesTabs = ['국내드라마', '미국드라마', '영국드라마', '일본드라마', '중국드라마'];
-  return (
-    <MainContent>
-      <MainBanner />
-      <MainContainer>
-        <TabButton tabs={seriesTabs} />
-        <PopularList />
+    const [loadMore, setLoadMore] = useState(0);
+    const seriesTabs = ['국내드라마', '미국드라마', '영국드라마', '일본드라마', '중국드라마'];
 
-        <CommonList />
-        <CommonList />
-        <GenreNav />
-        <CommonList />
-        <CommonList />
-        <Section>
-          <MoreButton>더보기</MoreButton>
-        </Section>
-      </MainContainer>
-    </MainContent>
-  );
+    const handleLoadMore = () => {
+        setLoadMore((prev) => prev + 3);
+    };
+
+    // 추가로 로드될 CommonList 컴포넌트들을 생성
+    const additionalLists = Array(loadMore)
+        .fill(null)
+        .map((_, index) => <CommonList key={`additional-${index}`} />);
+
+    return (
+        <MainContent>
+            <MainBanner />
+            <MainContainer>
+                <TabButton tabs={seriesTabs} />
+                <PopularList />
+                <CommonList />
+                <CommonList />
+                <GenreNav />
+                <CommonList />
+                <CommonList />
+                {additionalLists}
+                <Section>
+                    <MoreButton onClick={handleLoadMore}>더보기</MoreButton>
+                </Section>
+            </MainContainer>
+        </MainContent>
+    );
 };
 
 export default SeriesHome;
